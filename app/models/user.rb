9a38a7ad4bbe99a5252c9dd758669197b309152a
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-04-01 02:20:11 -0500

    AUTH tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  has_secure_password
+  validates :email, presence: true
+
+  def to_token_payload
+    {
+        sub: id,
+        email: email
+    }
+  end
+
 end

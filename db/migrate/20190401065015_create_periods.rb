602613bf1556d5841adcbaf2b96aecbe6a460ff4
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-04-01 01:51:30 -0500

    rails g Period

---
+++ db/migrate/20190401065015_create_periods.rb
@@ -0,0 +1,13 @@
+class CreatePeriods < ActiveRecord::Migration[5.2]
+  def change
+    create_table :periods do |t|
+      t.boolean :is_active
+      t.boolean :is_update
+      t.integer :duration
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end

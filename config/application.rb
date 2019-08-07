e2bcd9901837154c623595c6051b683fa9fd7f99
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-08-07 12:27:40 -0500

    CORS try

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module AutoobserverurBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:8100'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
